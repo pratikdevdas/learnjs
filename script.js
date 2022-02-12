06545781c9663edce1ef7a100c3620918a99d040
rev <dasdev.pratik@gmail.com>
2022-02-12 18:51:30 +0530

    init commit

---
+++ script.js
@@ -0,0 +1,12 @@
+alert('I am alert');
+
+let admin, name;
+name='John';
+admin=name;
+alert(admin)
+
+let checkName = "Ilya";
+
+alert(`hello${1}`)
+alert(`hello${"name"}`)
+alert(`hello${checkName}`)
